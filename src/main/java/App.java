ae042e1466e2a71f715b373cfe36eaea90723890
aradeh <harsh.arde@gmail.com>
2020-12-31 17:46:55 -0500

    reading input as string

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+
+public class App {
+    private static Scanner scan;
+
+    public static void main(String[] args) {
+        scan = new Scanner(System.in);
+        String zipCodeRanges = scan.nextLine();
+        System.out.println(zipCodeRanges);
+    }
+}
