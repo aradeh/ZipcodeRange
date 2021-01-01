295feda66f7765cccb5b8e111fe5eaf82e4189d7
aradeh <harsh.arde@gmail.com>
2020-12-31 21:09:08 -0500

    Execution code to get Zipcodes and call methods, store , sort, compute, and merge.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,21 @@ public class App {
     public static void main(String[] args) {
         scan = new Scanner(System.in);
         String zipCodeRanges = scan.nextLine();
-        System.out.println(zipCodeRanges);
+        //System.out.println(zipCodeRanges);
+
+        ZipcodeProcess zipcodeProcess = new ZipcodeProcess(zipCodeRanges);
+        //System.out.println(zipCodeRanges.toString());
+
+        List<Zipcode> zipcodeList = zipcodeProcess.splitZipcode();
+        ZipcodeMerge zipcodeMerge = new ZipcodeMerge();
+
+        List<Zipcode> sortedZipcode = zipcodeMerge.sortZipcodebyLower(zipcodeList);
+        List<Zipcode> mergedZipcode = zipcodeMerge.mergeZipcodes(sortedZipcode);
+
+        for(Zipcode zip: mergedZipcode){
+            System.out.println("[" + zip.getLower_zip() + "," + zip.getUpper_zip() + "]");
+        }
+
+
     }
 }
